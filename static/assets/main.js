ba368ed59397a72d48afcde682e9ed5ecb79b8a2
Brendon <60971135+brendon-liew@users.noreply.github.com>
2021-05-26 17:53:52 +0100

    Update main.js

--- static/assets/main.js
+++ static/assets/main.js
@@ -2,21 +2,43 @@ window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecogn
 
 
 
+
+
+
+
 const recognition = new window.SpeechRecognition();
+
+
+
+
 
 
 
 recognition.lang = "en-US";
 
+
+
 recognition.onresult = (e) => {
+
 				analyse( e.results[0][0].transcript.toLowerCase() );
+
 				var cap = e.results[0][0].transcript;
+
 				var running = document.getElementById("resultVoice");
+
 				running.value=cap;
+
 				var ac = document.getElementById("ac");
+
 				ac.value= e.results[0][0].confidence;
+
 				console.log('Confidence: ' + e.results[0][0].confidence);
+
 			       }
+
+
+
+
 
 
 
@@ -28,83 +50,172 @@ recognition.onerror = e => console.error( e );
 
 
 
+
+
+
+
+
+
+
+
 const go = where =>{
+
+
 
 	if( where.match(/forward|backward|left|right/) )
 
+
+
 		window.location.href = where;
 
+
+
 }
+
+
+
+
 
 
 
 const rules = [
 
+
+
 	{rule: /.*go(.*)/, response: 1, action: go }
+
+
 
 ];
 
 
 
+
+
+
+
 const analyse = msg => {
+
+
 
 	btn.disabled = false;
 
+
+
 	let response = "I didn't understand";
+
+
 
 	let matches;
 
+
+
 	for(let i = 0; i < rules.length; i++){
+
+
 
 		let r = rules[i];
 
+
+
 		matches = r.rule.exec(msg);
+
+
 
 		if( matches ){
 
+
+
 			response = r.response === 1 ? r.action(matches[1]) : r.response ;
+
+
 
 			break;
 
+
+
 		}
+
+
 
 	}
 
+
+
 	say(response);
 
+
+
 }
+
+
+
+
 
 
 
 const say = msg => {
 
+
+
 	let what = new SpeechSynthesisUtterance(msg);
+
+
 
 	what.lang = "en-US";
 
+
+
 	speechSynthesis.speak(what);
 
+
+
 }
+
+
+
+
 
 
 
 const btn = document.createElement('button');
 
+
+
 btn.innerText = "Speak";
 
-var btn2 = document.ElementByID("stopVoice");
+
+
+var btn2 = document.createElement('button');
+
+btn2.innerText = "Stop Speak";
+
 
 btn.onclick = ()=> {
 
+
+
 	btn.disabled = true;
+
+
 
 	recognition.start();
 
-}
-btn2.onclick=()=>{
-   btn.disabled = false;
-  recognition.stop();
+
+
 }
 
+btn2.onclick=()=>{
+
+   btn.disabled = false;
+
+  recognition.stop();
+
+}
+
+
+
 //document.body.appendChild(btn);
+
 document.getElementById("test").appendChild(btn);
+document.getElementById("test").appendChild(btn2);
